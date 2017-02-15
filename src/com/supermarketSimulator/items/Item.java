fe32bbbaaf390af92f79b3e50b218fe326d061e1
Josh Baird <jjbaird@oakland.edu>
2017-02-15 13:51:07 -0500

    Re-add ID to Item, but set from Database

--- src/com/supermarketSimulator/items/Item.java
+++ src/com/supermarketSimulator/items/Item.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 
 public class Item {
 	
+	private final int ID;
 	private final String name;
 	private final int baseHealth;
 	private final int baseHappiness;
@@ -11,7 +12,8 @@ public class Item {
 	private final double baseCost;
 	private final ImageIcon icon;
 	
-	public Item(String name, int health, int happiness, Category category, double cost, ImageIcon icon) {
+	public Item(int id, String name, int health, int happiness, Category category, double cost, ImageIcon icon) {
+		this.ID = id;
 		this.name = name;
 		this.baseHealth = health;
 		this.baseHappiness = happiness;
@@ -23,7 +25,7 @@ public class Item {
 	/**
 	 * Creates a new item from a string and adds it to the HashMap of all existing items
 	 *
-	 * @param itemString A comma-separated list of name, baseHealth, baseHappiness, category, cost
+	 * @param itemString A comma-separated list of id, name, baseHealth, baseHappiness, category, cost
 	 */
 	public static Item itemFromString(String itemString) {
 		String[] array = itemString.split(",");
@@ -33,7 +35,7 @@ public class Item {
 		} else {
 			category = Category.addCategory(array[3]);
 		}
-		return new Item(array[0], Integer.parseInt(array[1]), Integer.parseInt(array[2]), category, Double.parseDouble(array[3]), null); //TODO set icon
+		return new Item(Integer.parseInt(array[0]), array[1], Integer.parseInt(array[2]), Integer.parseInt(array[3]), category, Double.parseDouble(array[4]), null); //TODO set icon
 	}
 	
 	public ImageIcon getIcon() {
