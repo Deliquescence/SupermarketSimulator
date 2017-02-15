7ad0c73fdb17c49d9db43edc68266c2087b6d4e4
Isaac Springer <ispringer1996@gmail.com>
2017-02-15 14:26:16 -0500

    GUI changes 2/15/17

--- src/com/supermarketSimulator/GUI/ItemDisplay.java
+++ src/com/supermarketSimulator/GUI/ItemDisplay.java
@@ -2,6 +2,8 @@ package com.supermarketSimulator.GUI;
 
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ItemDisplay {
 	public JPanel panel;
@@ -21,5 +23,11 @@ public class ItemDisplay {
 		this.labelCost.setText("$" + String.format("%.2f", cost));
 		this.labelHealth.setText("♥" + health);
 		this.labelHappiness.setText("☺" + happiness);
+		buttonAdd.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+			}
+		});
 	}
 }
--- src/com/supermarketSimulator/GUI/MainGUI.java
+++ src/com/supermarketSimulator/GUI/MainGUI.java
@@ -6,6 +6,10 @@ public class MainGUI {
 	public JPanel mainPanel;
 	private JPanel rightPanel;
 	private JPanel leftPanel;
+	private JPanel topPanel;
+	private JTabbedPane tabbedPane2;
+	private JButton objectivesButton;
+	private JButton button1;
 	
 	private void populateItemList() {
 		//for testing add some hardcoded item display panels
