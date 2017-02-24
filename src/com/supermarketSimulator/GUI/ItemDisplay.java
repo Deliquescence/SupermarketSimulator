ecf6285c7ea1101b76c98519600dfa752341b669
Josh Baird <jjb.pub01@gmail.com>
2017-02-24 18:12:07 -0500

    Preparing for add button event handling

--- src/com/supermarketSimulator/GUI/ItemDisplay.java
+++ src/com/supermarketSimulator/GUI/ItemDisplay.java
@@ -15,11 +15,20 @@ public class ItemDisplay {
 	private JLabel labelHealth;
 	private JLabel labelHappiness;
 	
-	public ItemDisplay(Item item) {
+	/**
+	 * Create a new ItemDisplay for an Item.
+	 * Use the given ActionListener for the add button.
+	 *
+	 * @param item              the Item to display.
+	 * @param addButtonListener Action listener for the add button.
+	 */
+	public ItemDisplay(Item item, ActionListener addButtonListener) {
 		this.labelItemName.setText(item.getName());
 		this.labelCost.setText("$" + String.format("%.2f", item.getBaseCost()));
 		this.labelHealth.setText("♥" + item.getBaseHealth());
 		this.labelHappiness.setText("☺" + item.getBaseHappiness());
 		this.labelItemName.setIcon(item.getIcon());
+		
+		buttonAdd.addActionListener(addButtonListener);
 	}
 }
