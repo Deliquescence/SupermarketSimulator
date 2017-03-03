99cf1a5a0a74a108be4d096003e890ecfbfc302f
jakurOakland <jakur@oakland.edu>
2017-03-03 18:06:07 -0500

    Small improvement to Score.java

--- src/com/supermarketSimulator/game/Score.java
+++ src/com/supermarketSimulator/game/Score.java
@@ -1,7 +1,7 @@
 package com.supermarketSimulator.game;
 
 /**
- * Created by Gaming on 3/3/2017.
+ * Hosts static methods for scoring a shopping cart.
  */
 public class Score {
 	
@@ -20,16 +20,37 @@ public class Score {
 		/*
 		This will be broken down later
 		 */
-		double happinessScore = 0;
-		double healthScore = 0;
+		double happinessScore = scoreHappiness(cart);
+		double healthScore = scoreHealth(cart);
 		
-		if(cart.getHappinessTotal() * HAPPINESS_CONSTANT > 1) { //Avoiding massively negative answers
-			happinessScore = 100 * Math.log(HAPPINESS_CONSTANT * cart.getHappinessTotal());
-		}
-		
-		if(cart.getHealthTotal() * HEALTH_CONSTANT > 1) { //Avoiding massively negative answers
-			healthScore = 100 * Math.log(HEALTH_CONSTANT * cart.getHealthTotal());
-		}
 		return healthScore + happinessScore;
+	}
+	
+	/*
+	The below functions are public because I think we will want to display them to the user in the future
+	 */
+	
+	/**
+	 * Returns only happiness score of a cart
+	 * @param cart Shopping cart to score
+	 * @return Happiness score
+	 */
+	public static double scoreHappiness(ShoppingCart cart) {
+		if(cart.getHappinessTotal() * HAPPINESS_CONSTANT > 1) { //Avoiding massively negative answers
+			return 100 * Math.log(HAPPINESS_CONSTANT * cart.getHappinessTotal());
+		}
+		return 0;
+	}
+	
+	/**
+	 * Returns only health score of a cart
+	 * @param cart Shopping cart to score
+	 * @return Health score
+	 */
+	public static double scoreHealth(ShoppingCart cart) {
+		if(cart.getHealthTotal() * HEALTH_CONSTANT > 1) { //Avoiding massively negative answers
+			return 100 * Math.log(HEALTH_CONSTANT * cart.getHealthTotal());
+		}
+		return 0;
 	}
 }
