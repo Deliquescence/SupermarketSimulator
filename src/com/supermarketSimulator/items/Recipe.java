4a740d9f9d4040acfa8a269e398a5f8f4618bfdc
Josh Baird <jjbaird@oakland.edu>
2017-03-21 14:47:07 -0400

    Recipe: Add a static method for creation, rename recpie field
    We would have been doing things like aRecpieInstance.recpie, whereas
    now we do aRecpieInstance.ingredients
    This seems to make more sense to me

--- src/com/supermarketSimulator/items/Recipe.java
+++ src/com/supermarketSimulator/items/Recipe.java
@@ -9,15 +9,32 @@ public class Recipe {
 	
 	public static HashSet<Recipe> recipes = new HashSet<>();
 	
-	public RecipeStack[] recipe;
+	public RecipeStack[] ingredients;
 	
+	/**
+	 * Using a string with the format "Item#,Quantity,Item#,Quantity..." create a new Recipe.
+	 * Do not add it to the set of recipes.
+	 *
+	 * @param recipeString the String of data
+	 */
 	public Recipe(String recipeString) {
-		//Using format "Item#,Quantity,Item#,Quantity...
 		String[] split = recipeString.split(",");
-		recipe = new RecipeStack[split.length / 2];
-		for(int x = 0; x < split.length; x += 2) {
-			recipe[x / 2] = new RecipeStack(Database.items[Integer.parseInt(split[x])], Integer.parseInt(split[x+1]));
+		this.ingredients = new RecipeStack[split.length / 2];
+		for (int x = 0; x < split.length; x += 2) {
+			this.ingredients[x / 2] = new RecipeStack(Database.items[Integer.parseInt(split[x])], Integer.parseInt(split[x + 1]));
 		}
 	}
 	
+	/**
+	 * Using a string with the format "Item#,Quantity,Item#,Quantity..." create a new Recipe.
+	 * Add it to the set of recipes.
+	 *
+	 * @param recipeString the String of data
+	 * @return a Recipe with the given ingredients
+	 */
+	public static Recipe recipeFromString(String recipeString) {
+		Recipe r = new Recipe(recipeString);
+		recipes.add(r);
+		return r;
+	}
 }
